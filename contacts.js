75b859edde7311c8e3193d671107811fb0e17e1c
VitaBabenko <babenkovita1988@gmail.com>
2023-05-07 15:09:48 +0300

    in process

---
+++ contacts.js
@@ -0,0 +1,5 @@
+// const fs = require("fs").promises;
+
+// fs.readFile("./db/contacts.json")
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello vita");
+console.log("hello world");
+
+const fs = require("fs").promises;
+
+fs.readFile("./db/contacts.json")
+  .then((data) => console.log(data.toString()))
+  .catch((error) => console.log(error));
