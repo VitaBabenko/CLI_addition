fc720f6215eaad7f3929375cadd10770c5e75a13
VitaBabenko <babenkovita1988@gmail.com>
2023-05-09 00:11:15 +0300

    created function

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-console.log("hello world");
+const contactsService = require("./contacts.js");
 
-const fs = require("fs").promises;
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      console.log(allContacts);
+      break;
+    case "getContactById":
+      const oneContact = await contactsService.getContactById(id);
+      console.log(oneContact);
+      break;
+    case "addContact":
+      const newContact = await contactsService.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+    case "removeContact":
+      const removeContact = await contactsService.removeContact(id);
+      console.log(removeContact);
+      break;
+    default:
+      console.log("Unknown action");
+  }
+};
 
-fs.readFile("./db/contacts.json")
-  .then((data) => console.log(data.toString()))
-  .catch((error) => console.log(error));
+// invokeAction({ action: "list" });
+// invokeAction({ action: "getContactById", id: "AeHIrLTr6JkxGE6SN-0Rw" });
+// invokeAction({
+//   action: "add",
+//   name: "Platon Babenko",
+//   email: "babenkoplaton@mail.com",
+//   phone: "(987) 236 8800",
+// });
+invokeAction({ action: "removeContact", id: "Lygu99GwLzTtUL-CEGyLG" });
