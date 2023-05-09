f86e81f598917a140eebe8aafacef776757b7874
VitaBabenko <babenkovita1988@gmail.com>
2023-05-09 23:06:33 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const contactsService = require("./contacts.js");
+const { program } = require("commander");
+
+const contactsService = require("./contacts");
+
+program
+  .option("--a, --action <type>")
+  .option("--i, --id <type>")
+  .option("--n, --name <type>")
+  .option("--e, --email <type>")
+  .option("--p, --phone <type>");
+
+program.parse();
+
+const options = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -6,11 +19,11 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       const allContacts = await contactsService.listContacts();
       console.log(allContacts);
       break;
-    case "getContactById":
+    case "get":
       const oneContact = await contactsService.getContactById(id);
       console.log(oneContact);
       break;
-    case "addContact":
+    case "add":
       const newContact = await contactsService.addContact({
         name,
         email,
@@ -18,21 +31,13 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       });
       console.log(newContact);
       break;
-    case "removeContact":
+    case "remove":
       const removeContact = await contactsService.removeContact(id);
       console.log(removeContact);
       break;
     default:
-      console.log("Unknown action");
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "getContactById", id: "AeHIrLTr6JkxGE6SN-0Rw" });
-// invokeAction({
-//   action: "add",
-//   name: "Platon Babenko",
-//   email: "babenkoplaton@mail.com",
-//   phone: "(987) 236 8800",
-// });
-invokeAction({ action: "removeContact", id: "Lygu99GwLzTtUL-CEGyLG" });
+invokeAction(options);
